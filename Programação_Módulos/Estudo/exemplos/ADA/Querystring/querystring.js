8c27b238dcbbaf8fbdf86eccc82d950e99d31314
dhiogoFrutuoso <dhiogofrutuoso294@gmail.com>
2025-08-29 20:43:58 -0300

    url

--- Programação_Módulos/Estudo/exemplos/ADA/Querystring/querystring.js
+++ Programação_Módulos/Estudo/exemplos/ADA/Querystring/querystring.js
@@ -1,4 +1,5 @@
 import querystring from 'node:querystring';
+import url from 'node:url';
 
 const baseUrl = 'https://siteviagem.com.br'; //Cria uma url base
 
@@ -9,14 +10,15 @@ const uri = querystring.stringify({ //Cria um objeto com o querystring
     ano: '2025'
 });
 
-const url = `${baseUrl}/${uri}`; //Junta a url base com o uri, fazendo o url completo
+const fullUrl = `${baseUrl}/${uri}`; //Junta a url base com o uri, fazendo o url completo
 
-console.log(url); 
+console.log(fullUrl); 
 
 const parsedUrl = querystring.parse(uri); //Faz o processo inverso, transformando o uri em um objeto novamente
 console.log(uri); //Printa a uri (destino=RJ&periodo=Verao&horario=noite&ano=2025)
 console.log(parsedUrl); //Printa a mesma uri, mas em formato de objeto
 console.log(parsedUrl.destino); //Printa o valor do destino (RJ)
+console.log('fullUrl: ', url.parse(fullUrl)); //Printa a url completa em formato de objeto
 
 const uri2 = querystring.escape('estado= são paulo&cidade= são mateus'); //cria uma uri escapada, escrevendo em formato de url
 console.log(uri2); 
