fc2ef1b139182d7db7044d6e0e52b8cf8d10bb6e
dhiogoFrutuoso <dhiogofrutuoso294@gmail.com>
2025-09-25 19:21:28 -0300

    paste arquives in exemplos

---
+++ Programação_Módulos/Estudo/exemplos/NodeJs/Express/Formulário/models/Post.js
@@ -0,0 +1,13 @@
+import db from './db.js'
+
+const Post = db.sequelize.define('post', {
+    title: {
+        type: db.Sequelize.STRING
+    },
+    content: {
+        type: db.Sequelize.TEXT
+    }
+});
+
+Post.sync({force: false}); //Já foi definido a tabela, false evita que a tabela seja sempre recriada quando o código for executado.
+export default Post;
